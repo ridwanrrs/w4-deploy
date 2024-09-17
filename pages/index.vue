603a7db46774db7f47b57e252f9ea2362f5a954a
Rivo Juicer Wowor <rivowowor7@gmail.com>
2024-09-17 10:30:38 +0700

    init: nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+let name = process.env.STUDENT_NAME ?? "Agus"
+let nim = process.env.STUDENT_NIM ?? "00000012345"
+</script>
+
+<template>
+  <div class="min-h-screen w-full flex flex-col gap-4 items-center justify-center bg-gray-900 text-white">
+    <h1 class="font-bold text-6xl">Hello World!</h1>
+    <p class="text-2xl">{{ name }} - {{ nim }}</p>
+  </div>
+</template>
