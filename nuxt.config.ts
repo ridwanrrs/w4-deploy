6558969c5e170f01bb480ea67888e8028e05a5a4
ivanWib <christ.ivan5758@gmail.com>
2024-09-18 14:34:23 +0700

    Fix : Nuxt Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     // Make environment variables accessible here
     public: {
-      STUDENT_NAME: process.env.STUDENT_NAME,
-      STUDENT_NIM: process.env.STUDENT_NIM,
+      STUDENT_NAME: process.env.NUXT_STUDENT_NAME,
+      STUDENT_NIM: process.env.NUXT_STUDENT_NIM,
       // Add more environment variables as needed
     },
   },
